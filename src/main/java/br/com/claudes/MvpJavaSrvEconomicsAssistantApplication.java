dbd09e3824b1c0fe4ae42ea7ccc39624a40bbb40
Claudes dos Santos Ferreira Gomes <claudesferreira99@gmail.com>
2024-01-22 13:34:35 -0300

    first commit

---
+++ src/main/java/br/com/claudes/MvpJavaSrvEconomicsAssistantApplication.java
@@ -0,0 +1,13 @@
+package br.com.claudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpJavaSrvEconomicsAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpJavaSrvEconomicsAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/claudes/MvpJavaSrvEconomicsAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.claudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpJavaSrvEconomicsAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
